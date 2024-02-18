76cbb44dc1f0ffee9f109b7ef69c6880f6ccd079
HeinKoKoAung <m71343333@gmail.com>
2024-02-18 20:01:30 +0630

    first commit

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _showInfoMessage(String message){
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
+
+  void _toggleMealFavoriteStatus(Meal meal){
+    final bool isExisting = _favoriteMeals.contains(meal);
+
+    if(!isExisting){
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showInfoMessage('Marked as a favourite');
+    }else{
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favourite.');
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  void _setScreen(String identifier){
+    if(identifier == 'filters'){
+      
+    }else{
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage =  CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus,);
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePageTitle = 'Your Favourite';
+      activePage =  MealsScreen( title: activePageTitle,meals: _favoriteMeals,onToggleFavorite: _toggleMealFavoriteStatus);
+      
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text(activePageTitle)),
+      drawer:  MainDrawer(onSelectScreen: _setScreen,),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(
+                icon: Icon(Icons.star), label: 'Favourite')
+        ],
+      ),
+    );
+  }
+}
